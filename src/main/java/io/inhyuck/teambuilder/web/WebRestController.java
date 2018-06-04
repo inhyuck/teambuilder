0ad4d6a342b2403b3890d8cc1bbbe4eca298c292
INHYCUK <dlsgur1447@gmail.com>
2018-06-05 01:23:10 +0900

    프로젝트 생성

---
+++ src/main/java/io/inhyuck/teambuilder/web/WebRestController.java
@@ -0,0 +1,13 @@
+package io.inhyuck.teambuilder.web;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class WebRestController {
+
+    @GetMapping("/hello")
+    public String hello() {
+        return "HelloWorld";
+    }
+}
