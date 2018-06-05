0e0c88f2082c0a35e6db0b6adb2882254018bc8a
INHYCUK <dlsgur1447@gmail.com>
2018-06-05 19:36:11 +0900

    [add] posts/Posts | posts/PostsRepository

---
+++ src/main/java/io/inhyuck/teambuilder/domain/posts/PostsRepository.java
@@ -0,0 +1,6 @@
+package io.inhyuck.teambuilder.domain.posts;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface PostsRepository extends JpaRepository<Posts, Long> {
+}
